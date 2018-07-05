7618b6b375eba607dfd453936584506a0e2c98f5
s1240190 <s1240190@std5dc20.u-aizu.ac.jp>
2018-07-05 13:52:02 +0900

    Won/lost message added

--- diceroll.c
+++ diceroll.c
@@ -6,11 +6,12 @@ int main(){
   char enter1,enter2;
   int i;
   int input1,input2;
-
+  int total;
+  
   srand((unsigned)time(NULL));
 
   printf("Rolling the dice...\n");
-  printf("Press enter key two times: ");
+  printf("Press enter key... ");
   scanf("%c",&enter1);
   scanf("%c",&enter2);
   if(enter1 == '\n' && enter2 == '\n'){
@@ -20,7 +21,11 @@ int main(){
       printf("Die 1: %d\n",input1);
       printf("Die 2: %d\n",input2);
     }
-    printf("Total Value: %d\n",input1+input2);
+    total = input1+input2;
+    printf("Total Value: %d\n",total);
+    if(total >= 8)printf("You won\n");
+    else printf("You lost\n");
+	       
   }
   else printf("Press enter key\n");
 
